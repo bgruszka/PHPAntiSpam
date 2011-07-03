e45898565ab0a065ea6f732482a22373c58197ff
snipe <blazej@gruszka.info>
2011-07-03 20:20:18 +0200

    added getMessages function to get messages from files

--- example.php
+++ example.php
@@ -3,30 +3,35 @@
 require_once 'lib/antispam.class.php';
 require_once 'lib/corpus.class.php';
 
-$dirs = array('spam', 'nospam');
-
-$messages = array();
-
-foreach($dirs as $dir) {
-    $files = glob($dir.'/*.txt');
-
-    foreach($files as $file) {
-    	$message = array();
-    	
-		$content = file_get_contents($file);
-        $content = str_replace(
-            array('.', ',', ';', '"', ':', '?', '!', '+', '-', '/', '*', '=', '<', '>', '|', '&', '~', '`', '@'),
-            ' ',
-            $content
-        );
-
-        $message['content'] = $content;
-        $message['category'] = $dir;
-        
-        $messages[] = $message;
-    }
+function getMessages() {
+	$dirs = array('spam', 'nospam');
+	
+	$messages = array();
+	
+	foreach($dirs as $dir) {
+	    $files = glob($dir.'/*.txt');
+	
+	    foreach($files as $file) {
+	    	$message = array();
+	    	
+			$content = file_get_contents($file);
+	        $content = str_replace(
+	            array('.', ',', ';', '"', ':', '?', '!', '+', '-', '/', '*', '=', '<', '>', '|', '&', '~', '`', '@'),
+	            ' ',
+	            $content
+	        );
+	
+	        $message['content'] = $content;
+	        $message['category'] = $dir;
+	        
+	        $messages[] = $message;
+	    }
+	}
+	
+	return $messages;
 }
 
+$messages = getMessages();
 $corpus = new Corpus($messages);
 $antispam = new Antispam($corpus);
 
