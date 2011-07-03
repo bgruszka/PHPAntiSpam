f943db88aaaf70104ed5e4a96e397687ed2593ed
snipe <blazej@gruszka.info>
2011-07-03 16:50:05 +0200

    cleaning

--- lib/antispam.class.php
+++ lib/antispam.class.php
@@ -1,12 +1,15 @@
 <?php
 
-class Antispam {
-	public static function graham($sh, $ih, $ts, $ti) {
+class Antispam 
+{
+	public static function graham($sh, $ih, $ts, $ti) 
+	{
 		$p = ($sh/$ts)/(($sh/$ts) + ((2*$ih)/$ti));
 		return $p;
 	}
 	
-	public static function robinson($n, $graham) {
+	public static function robinson($n, $graham) 
+	{
 		$s = 1;
 		$x = 0.5;
 		$fw = ($s*$x + $n*$graham) / ($s + $n);
