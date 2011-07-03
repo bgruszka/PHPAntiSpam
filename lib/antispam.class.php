8a004fbf12881ba82d851e3dcebd93b68b73d6a0
snipe <blazej@gruszka.info>
2011-07-03 19:12:04 +0200

    added corpus and antispam classes

--- lib/antispam.class.php
+++ lib/antispam.class.php
@@ -16,6 +16,42 @@ class Antispam
 	
 		return $fw;
 	}
+	
+	public static function isSpam($text, Corpus $corpus)
+	{
+		$words = explode(' ', $text);
+		
+		foreach($words as $word) {
+			$word = trim($word);
+			if(strlen($word) > 0) {
+				if(!isset($corpus->corpusList[$word])) {
+					$probability = 0.5;
+				} else {
+					$probability = $corpus->corpusList[$word]['probability'];
+				}
+				
+				$przydatnosc = abs(0.5 - $probability);
+				$needed[$word]['probability'] = $probability;
+				$needed[$word]['przydatnosc'] = $przydatnosc;
+				$przydatnosciArray[] = $przydatnosc;
+			}
+		}
+		
+		array_multisort($przydatnosciArray, SORT_DESC, $needed);
+		
+		$needed = array_slice($needed, 0, 15);
+		
+		$licznik = 1;
+		$mianownik = 1;
+		foreach($needed as $word) {
+			$licznik *= $word['probability'];
+			$mianownik *= 1 - $word['probability'];
+		}
+		
+		$result = $licznik / ($licznik + $mianownik);
+		
+		return $result;
+	}
 }
 
 ?>
---
+++ lib/corpus.class.php
@@ -0,0 +1,50 @@
+<?php
+
+class Corpus
+{
+	
+	protected $messages = array();
+	public $corpusList = array();
+	public $messagesCount = array('spam' => 0, 'nospam' => 0);
+	
+	public function __construct($messages)
+	{
+		$this->messages = $messages;
+		
+		foreach($this->messages as $message) {
+			$this->messagesCount[$message['category']]++;
+			
+			$message['content'] = str_replace(
+				array(
+					'.', ',', ';', '"', ':', '?', 
+					'!', '+', '-', '/', '*', '=', 
+					'<', '>', '|', '&', '~', '`', '@'
+				), 
+				' ',
+				$message['content']
+		    );
+		    
+			$words = explode(' ', $message['content']);
+		
+			foreach($words as $key => $word) {
+				$word = strtolower(trim($word));
+				if(strlen($word) > 4) {
+					if(!empty($word)) {
+						if(in_array($word, array_keys($this->corpusList))) {
+								$this->corpusList[$word][$message['category']]++;
+						} else {
+							if($message['category'] == 'spam') {
+								$this->corpusList[$word] = array('spam' => 1, 'nospam' => 0);
+							} else {
+								$this->corpusList[$word] = array('spam' => 0, 'nospam' => 1);
+							}
+						}
+					}
+				}
+			}
+		}	
+	}
+	
+}
+
+?>
