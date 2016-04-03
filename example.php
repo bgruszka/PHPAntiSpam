d2f6857b51f95e3f21860076cf639cdc32615a5e
bgruszka <github@gruszka.info>
2016-04-03 11:13:00 +0200

    cleaning example

--- example.php
+++ example.php
@@ -6,45 +6,18 @@ use PHPAntiSpam\Corpus;
 use PHPAntiSpam\AntiSpam;
 use PHPAntiSpam\Tokenizer\WhitespaceTokenizer;
 
-function getMessages() {
-	$dirs = array('spam', 'nospam');
-	
-	$messages = array();
-	
-	foreach($dirs as $dir) {
-	    $files = glob($dir.'/*.txt');
-	
-	    foreach($files as $file) {
-	    	$message = array();
-	    	
-			$content = file_get_contents($file);
-	
-	        $message['content'] = $content;
-	        $message['category'] = $dir;
-	        
-	        $messages[] = $message;
-	    }
-	}
-	
-	return $messages;
-}
+$messages = [
+    ['category' => 'spam', 'content' => 'this is spam'],
+    ['category' => 'nospam', 'content' => 'this is'],
+];
 
-$messages = getMessages();
 $tokenizer = new WhitespaceTokenizer();
 $corpus = new Corpus($messages, $tokenizer);
+
 $antispam = new AntiSpam($corpus);
-$antispam->setMethod(new \PHPAntiSpam\Method\BurtonMethod($corpus));
+$antispam->setMethod(new \PHPAntiSpam\Method\GrahamMethod($corpus));
 
-$message = 'This promotion is sponsored exclusively by Vindale Research';
+$spamProbability = $antispam->isSpam('This is spam');
 
-$spamProbability = $antispam->isSpam($message);
-
-var_dump($spamProbability);
-
-if($spamProbability < 0.9) {
-	echo PHP_EOL . 'no spam' . PHP_EOL;
-} else {
-	echo PHP_EOL . 'spam' . PHP_EOL;
-}
-
-?>
+echo sprintf('Spam probability: %s', $spamProbability) . PHP_EOL;
+echo sprintf('Is spam: %s', $spamProbability < 0.9 ? 'NO' : 'YES') . PHP_EOL;
